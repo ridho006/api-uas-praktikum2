a8999c7187b2b3317ad9f77af3b5a40a9566a558
ridho006 <ridho.aiman006@gmail.com>
2025-11-29 20:50:21 +0700

    baruu

--- server.js
+++ server.js
@@ -210,6 +210,21 @@ app.post('/directors', authenticateToken, async (req, res, next) => {
     }
 });
 
+app.put('/directors/:id', [authenticateToken, authorizeRole('admin')], async (req, res, next) => {
+    const { name, birthYear } = req.body;
+    const sql = 'UPDATE directors SET name = $1, birthYear = $2, WHERE id = $3 RETURNING *';
+    
+    try {
+        const result = await db.query(sql, [name, birthYear, req.params.id]);
+        if (result.rowCount === 0) {
+            return res.status(404).json({ error: 'Director tidak ditemukan' });
+        }
+        res.json(result.rows[0]);
+    } catch (err) {
+        next(err);
+    }
+});
+
 // === FALLBACK & ERROR HANDLING ===
 app.use((req, res) => {
     res.status(404).json({ error: 'Rute tidak ditemukan' });
